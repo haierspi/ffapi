3b9aa0895982bb653b69c03c60d4496304314dc9
曹广来 <haierspi@qq.com>
2020-04-29 14:37:53 +0800

    修正错误提示

--- common/core/code/SussedCode.php
+++ common/core/code/SussedCode.php
@@ -7,7 +7,7 @@ namespace ff\code;
 class SussedCode extends BaseCode
 {
     const SUSSED = 1;
-    const FAILED = 1;
+    const FAILED = 0;
 
     const SUSSED_MSG = 'Sussed!';
     const FAILED_MSG = 'Failed!';
